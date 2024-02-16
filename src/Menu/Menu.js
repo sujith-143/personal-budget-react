a395bb0556e95e0482118f54b8ac594cfdd28a84
sujith-143 <sujithari143@gmail.com>
2024-02-16 16:26:21 -0500

    Brought PBudget code in

---
+++ src/Menu/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  Link
+} from "react-router-dom";
+
+
+function Menu() {
+  return (
+    <div class="menu">
+        <ul>
+            <li><Link to="/">Homepage</Link></li>
+            <li><Link to="about">About</Link></li>
+            <li><Link to="login">Login</Link></li>
+            
+        </ul>
+    </div>
+  );
+}
+
+export default Menu;
